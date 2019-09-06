88242c740f6527ff51f5536107890cd4360b45f8
Christian <christianmontero08@gmail.com>
2019-09-06 12:32:12 -0700

    Setup app for Heroku

--- server/server.js
+++ server/server.js
@@ -7,10 +7,11 @@ var { User } = require('./models/user')
 const { ObjectID } = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${ port }`);
 });
 
 // app.post('/todos', (req, res) => {
